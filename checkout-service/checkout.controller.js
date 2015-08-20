d3feb946505e7f049ed0ff450d84b5e675fe4c13
Ruben Oostinga <roostinga@xebia.com>
2015-08-20 11:17:05 +0200

    remove service

--- checkout-service/checkout.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('clientsideIntegration')
-  .controller('CheckoutCtrl', function () {
-    this.message = 'This controller is lazy loaded from a different service.';
-  });
